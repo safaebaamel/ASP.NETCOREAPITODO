e1a0f2696c83dd86196c4814538f2109a9ff557e
Safae Baamel <sapaebaamel@gmail.com>
2021-11-18 15:50:32 +0100

    updating the program.cs file,adding async functions

--- Program.cs
+++ Program.cs
@@ -8,77 +8,92 @@ var connectionString = builder.Configuration["ConnectionStrings:DefaultConnectio
 builder.Services.AddDbContext<ApiDbContext>(options => 
     options.UseSqlite(connectionString));
 
-builder.Services.AddSingleton<ItemRepository>();
+// builder.Services.AddSingleton<ItemRepository>();
+
 var app = builder.Build();
 
-app.MapGet("/items", ([FromServices] ItemRepository items) => {
-    return items.GetAll();
+app.MapGet("/items", async (ApiDbContext db) => {
+    return await db.Items.ToListAsync();
 });
 
-app.MapPost("/items", ([FromServices] ItemRepository items, Item item) => {
-    if(items.GetById(item.id) != null) {
+app.MapPost("/items", async (ApiDbContext db, Item item) => {
+    if(await db.Items.FirstOrDefaultAsync(x => x.Id == item.Id) != null) {
         return Results.BadRequest();
     } 
-    items.Add(item);
-    return Results.Created($"/Items/{item.id}", item);
+    
+    db.Items.Add(item);
+    await db.SaveChangesAsync();
+    return Results.Created($"/Items/{item.Id}", item);
 });
 
-app.MapGet("/items/{id}", ([FromServices] ItemRepository items, int id) => {
-    var item = items.GetById(id);
+app.MapGet("/items/{id}", async (ApiDbContext db, int id) => {
+    var item = await db.Items.FirstOrDefaultAsync(x => x.Id == id);
     return item == null ? Results.NotFound() : Results.Ok(item);
 });
 
-app.MapPut("/items/{id}", ([FromServices] ItemRepository items, int id, Item item) => {
-    if(items.GetById(id) == null) {
+app.MapPut("/items/{id}", async (ApiDbContext db, int id, Item item) => {
+    var existItem =  await db.Items.FirstOrDefaultAsync(x => x.Id == id);
+    if(existItem == null) {
         return Results.NotFound();
     }
-    items.Update(item);
+    existItem.title = item.title;
+    existItem.IsCompleted = item.IsCompleted;
+
+    await db.SaveChangesAsync();
     return Results.Ok(item);
 });
 
-app.MapDelete("/items/{id}", ([FromServices] ItemRepository items, int id) => {
-    if(items.GetById(id) == null) {
+app.MapDelete("/items/{id}", async (ApiDbContext db, int id) => {
+    var existItem = await db.Items.FirstOrDefaultAsync(x => x.Id == id); 
+    if(existItem == null) {
         return Results.NotFound();
     }
-    items.Delete(id);
+    db.Items.Remove(existItem);
+    await db.SaveChangesAsync();
     return Results.NoContent();
 });
 
 app.MapGet("/", () => "Hello From Minimal Api");
 app.Run();
 
-record Item(int id, string title, bool IsCompleted);
+// record Item(int id, string title, bool IsCompleted);
 
-class ItemRepository 
-{
-    private Dictionary<int, Item> items = new Dictionary<int, Item>();
-
-    public ItemRepository()
-    {
-        var item1 = new Item(1, "Buy Groceries ", false);
-        var item2 = new Item(2, "Drink 1L of water ", true);
-        var item3 = new Item(3, "Pay bills ", false);
-        items.Add(item1.id, item1);
-        items.Add(item2.id, item2);
-        items.Add(item3.id, item3);
-    }
-
-    public IEnumerable<Item> GetAll() => items.Values;
-
-    public Item? GetById(int id) {
-      if(items.ContainsKey(id)) {
-        return items[id];
-      }
-      return null;
-    }
-
-    public void Add(Item item) => items.Add(item.id, item);
-
-    public void Update(Item item) => items[item.id] = item;
-
-    public void Delete(int id) => items.Remove(id);
-
+class Item {
+    public int Id { get; set; }
+    public string? title { get; set; }
+    public bool IsCompleted { get; set; }
 }
+
+// class ItemRepository 
+// {
+    // private Dictionary<int, Item> items = new Dictionary<int, Item>();
+
+    // public ItemRepository()
+    // {
+    //     var item1 = new Item(1, "Buy Groceries ", false);
+    //     var item2 = new Item(2, "Drink 1L of water ", true);
+    //     var item3 = new Item(3, "Pay bills ", false);
+    //     items.Add(item1.id, item1);
+    //     items.Add(item2.id, item2);
+    //     items.Add(item3.id, item3);
+    // }
+
+    // public IEnumerable<Item> GetAll() => items.Values;
+
+    // public Item? GetById(int id) {
+    //   if(items.ContainsKey(id)) {
+    //     return items[id];
+    //   }
+    //   return null;
+    // }
+
+    // public void Add(Item item) => items.Add(item.id, item);
+
+    // public void Update(Item item) => items[item.id] = item;
+
+    // public void Delete(int id) => items.Remove(id);
+
+// }
 
 class ApiDbContext: DbContext
 {
